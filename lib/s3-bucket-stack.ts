847b8436d59fc4e0551de9d10a151c6a071370b3
Furkan Perçin <furkanpercin@hotmail.com>
2022-06-06 23:58:41 +0300

    CDK S3 Bucket

--- lib/s3-bucket-stack.ts
+++ lib/s3-bucket-stack.ts
@@ -3,18 +3,15 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from 'constructs';
 
 export class S3BucketStack extends cdk.Stack {
-  public readonly bucket: s3.Bucket; //Resource sharing between stacks
+ // public readonly bucket: s3.Bucket; //Resource sharing between stacks
 
   
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines the s3 stack 
-    this.bucket = new s3.Bucket(this, "patika-bucket",{//Resource logical ID
-      versioned: false,
+    new s3.Bucket(this, "patika-bucket",{//Resource physical ID
       bucketName: "patika-bucket",
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
     
